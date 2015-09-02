bd0f0facb62d6192fb2190463eaa046f5687d43f
Takeo Kunishima <t.kunishi@gmail.com>
2015-09-02 16:51:58 +0900

    install openssl library.

--- cookbooks/vagrant-virtuoso/recipes/default.rb
+++ cookbooks/vagrant-virtuoso/recipes/default.rb
@@ -9,3 +9,11 @@ include_recipe 'build-essential'
 include_recipe 'git'
 
 package 'gperf'
+package 'Install OpenSSL dev' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'openssl-dev'
+  when 'ubuntu', 'debian'
+    package_name 'libssl-dev'
+  end
+end
