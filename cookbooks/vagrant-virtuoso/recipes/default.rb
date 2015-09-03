7904bd2f2e135522a4b497f7175dc31688919397
Takeo Kunishima <t.kunishi@gmail.com>
2015-09-03 15:21:44 +0900

    compile virtuoso.

--- cookbooks/vagrant-virtuoso/recipes/default.rb
+++ cookbooks/vagrant-virtuoso/recipes/default.rb
@@ -4,11 +4,15 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+configure_options = '--prefix=/usr/local --with-layout=debian'
+src_dir = '/usr/local/src/virtuoso-opensource'
+
 include_recipe 'apt'
 include_recipe 'build-essential'
 include_recipe 'git'
 
 package 'gperf'
+package 'libtool'
 package 'Install OpenSSL dev' do
   case node[:platform]
   when 'redhat', 'centos'
@@ -21,3 +25,55 @@ package 'libiodbc2'
 package 'libiodbc2-dev'
 package 'openjdk-7-jdk'
 package 'python-dev'
+
+git src_dir do
+  repository 'git://github.com/openlink/virtuoso-opensource.git'
+  action :sync
+  notifies :run, 'execute[cleanup-virtuoso-source-dir]', :immediately
+end
+
+execute 'cleanup-virtuoso-source-dir' do
+  cwd src_dir
+  command 'make distclean'
+  action :run
+  only_if do File.exists?("#{src_dir}/Makefile") end
+  notifies :run, 'execute[autogen-virtuoso]', :immediately
+end
+
+execute 'autogen-virtuoso' do
+  cwd src_dir
+  command './autogen.sh'
+  action :run
+  only_if do File.exists?("#{src_dir}/autogen.sh") end
+  notifies :run, 'execute[configure-virtuoso]', :immediately
+end
+
+execute 'configure-virtuoso' do
+  cwd src_dir
+  command "./configure #{configure_options}"
+  action :nothing
+  only_if do File.exists?("#{src_dir}/configure") end
+  notifies :run, 'execute[compile-virtuoso]', :immediately
+end
+
+execute 'compile-virtuoso' do
+  cwd src_dir
+  command 'make'
+  action :nothing
+  only_if do File.exists?("#{src_dir}/Makefile") end
+  notifies :run, 'execute[install-virtuoso]', :immediately
+end
+
+execute 'install-virtuoso' do
+  cwd src_dir
+  command 'make install'
+  only_if do File.exists?("#{src_dir}/binsrc/virtuoso/virtuoso-t") end
+  action :nothing
+  notifies :run, 'execute[run-virtuoso]', :delayed
+end
+
+execute 'run-virtuoso' do
+  command 'isql 1111 dba dba -K'
+  command 'virtuoso-t'
+  action :nothing
+end
