a63f03c1c35398cfc7e8ec3a029d43194d77d07b
Takeo Kunishima <t.kunishi@gmail.com>
2015-09-11 14:18:12 +0900

    rename recipe to virtuoso-opensource-7.

--- cookbooks/virtuoso-opensource-7/spec/unit/recipes/default_spec.rb
+++ cookbooks/virtuoso-opensource-7/spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook Name:: vagrant-virtuoso
+# Cookbook Name:: virtuoso-opensource-7
 # Spec:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'vagrant-virtuoso::default' do
+describe 'virtuoso-opensource-7::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
